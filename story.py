14ea16894c3450147d14f89bd4dfbe73ccb85b26
JanRkck <85585594+JanRkck@users.noreply.github.com>
2025-07-16 18:06:53 +0200

    Update story.py
    inhalt vom Pi hinzugefügt

--- story.py
+++ story.py
@@ -1,1 +1,26 @@
+# story.py
+SCENES = [
+    {"audio": "titel.mp3"},
+    {"audio": "prolog.mp3"},
+
+    # Checkpoint 1
+    {"audio": "checkpoint1.mp3",
+     "lcd":  ("5 Segel +",      # line-1 
+              "3 Hute = ?"),    # line-2
+     "answer": 8,               #richtige antwort
+     "right":  "richtig1.mp3"}, #audio
+
+    {"audio": "storyweiter.mp3"},
+
+    # Checkpoint 2
+    {"audio": "checkpoint2.mp3",
+     "lcd":  ("10 Munzen - ",   # line-1
+              "4 Munzen = ?"),  # line-2
+     "answer": 6,
+     "right":  "richtig2.mp3"},
+
+    {"audio": "ende.mp3"}
+]
+
+WRONG_WAV = "falsch.mp3"          #jede falsche antwort
 
